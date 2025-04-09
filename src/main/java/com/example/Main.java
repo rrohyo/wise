5b8f9d87c407be5d1b44bc424f5efe94ae709fbc
rrohyo <aoslwjs2926@gmail.com>
2025-04-09 20:14:01 +0900

    종료기능 구현

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,15 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("== motivation 앱 실행 ==");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("명령어) ");
-        String command = scanner.nextLine();
-
-
-        scanner.close();
-
-        System.out.println("test");
-
 
         }
     }
