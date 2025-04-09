2e81072275911cad3a10cbd4d4a70b2fb6816b1b
rrohyo <aoslwjs2926@gmail.com>
2025-04-09 19:58:56 +0900

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("== motivation 앱 실행 ==");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("명령어) ");
+        String command = scanner.nextLine();
+
+
+        scanner.close();
+
+
+        }
+    }
