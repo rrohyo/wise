5fcfa038e16fe5d255e23b1fab83b212ac8e9cc8
rrohyo <aoslwjs2926@gmail.com>
2025-04-09 21:31:02 +0900

    4단계, WiseSaying 클래스 생성, List에 명언 저장

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,37 +1,53 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
     private Scanner sc;
 
-    App(Scanner sc) {
+    App(Scanner sc){
         this.sc = sc;
     }
 
-    public void run() {
+    public void run(){
         System.out.println("== motivation 앱 실행 == ");
+
         int lastId = 1;
-        // 혹시 모를 공백 제거 : trim()
-        while (true) {
+        int listCount = 0;
+        List<WiseSaying> wiseSayingList = new ArrayList<>();
+
+        while(true){
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
 
-            if (cmd.equals("add")) {
+            if(cmd.equals("add")){
                 System.out.print("명언 : ");
                 String content = sc.nextLine();
                 System.out.print("인물 : ");
                 String person = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
+
+                WiseSaying temp = new WiseSaying(lastId, content, person);
+                wiseSayingList.add(temp);
+
                 lastId++;
+                listCount++;
+            }
+            else if(cmd.equals("list")){
+                System.out.printf("등록된 명언 수 : %d\n", listCount);
             }
 
-            if (cmd.equals("exit")) {
-
+            else if (cmd.equals("exit")){
+                System.out.println("== motivation 앱 종료 ==");
+                break;
             }
+            else {
+                System.out.println("존재하지 않는 명령어입니다.");
+            }
+
         }
-
     }
 }
-
