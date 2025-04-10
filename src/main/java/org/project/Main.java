c34642188234a064227e5c48e16cfa870d4953c2
rrohyo <aoslwjs2926@gmail.com>
2025-04-10 20:20:11 +0900

    SystemController, WiseSayingController로 기능 분리

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -2,8 +2,6 @@ package org.project;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +10,6 @@ public class Main {
         new App(sc).run();
 
         sc.close();
+
     }
 }
--- src/main/java/org/wiseSaying/entity/WiseSaying.java
+++ src/main/java/org/wiseSaying/entity/WiseSaying.java
@@ -35,4 +35,3 @@ public class WiseSaying {
         this.person = person;
     }
 }
-
